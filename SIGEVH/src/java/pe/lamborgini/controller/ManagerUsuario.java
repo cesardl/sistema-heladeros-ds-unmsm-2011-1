c11e58bdbf3563a8e5791bb852b266d2fc658e21
cesar.dl88@gmail.com <cesar.dl88@gmail.com@c57c525a-3481-9d06-e16f-5780e9421b25>
2011-06-29 15:08:55 +0000

    Actualizacion de la interfaz para mantenimiento de heladeros

--- SIGEVH/src/java/pe/lamborgini/controller/ManagerUsuario.java
+++ SIGEVH/src/java/pe/lamborgini/controller/ManagerUsuario.java
@@ -4,6 +4,8 @@
  */
 package pe.lamborgini.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import pe.lamborgini.domain.Usuario;
 import pe.lamborgini.service.UsuarioService;
 
@@ -14,6 +16,7 @@ import pe.lamborgini.service.UsuarioService;
 public class ManagerUsuario {
 
     private Usuario usuario;
+    private List<Usuario> listaUsuarios;
 
     public ManagerUsuario() {
         this.usuario = new Usuario();
@@ -27,15 +30,34 @@ public class ManagerUsuario {
         this.usuario = usuario;
     }
 
+    public List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
     public String ingresar() {
         System.out.println("Bienvenido " + usuario);
         if (UsuarioService.validarUsuario(usuario)) {
+            listaUsuarios = new ArrayList<Usuario>();
+            listaUsuarios.add(new Usuario("user1", "passwd1"));
+            listaUsuarios.add(new Usuario("user2", "passwd2"));
+            listaUsuarios.add(new Usuario("user3", "passwd3"));
+            listaUsuarios.add(new Usuario("user4", "passwd4"));
+            listaUsuarios.add(new Usuario("user5", "passwd5"));
+            listaUsuarios.add(new Usuario("user6", "passwd6"));
+            listaUsuarios.add(new Usuario("user7", "passwd7"));
+            listaUsuarios.add(new Usuario("user8", "passwd8"));
+            listaUsuarios.add(new Usuario("user9", "passwd9"));
+            System.out.println("se inicializo la lista " + listaUsuarios.size());
             return "SUCCESS";
         } else {
             return "FAIL";
         }
     }
-    
+
     public String salir() {
         return "TO_INDEX";
     }
---
+++ SIGEVH/web/Heladero/asignar_helados.jsp
@@ -0,0 +1,16 @@
+<%-- 
+    Document   : asignar_helados
+    Created on : 19/06/2011, 02:50:04 PM
+    Author     : Cesardl
+--%>
+
+<%@taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
+<%@taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
+<%@taglib prefix="rich" uri="http://richfaces.org/rich"%>
+<%@taglib prefix="a4j" uri="http://richfaces.org/a4j"%>
+
+<f:subview id="asignar_helados">
+    <h:form>
+
+    </h:form>
+</f:subview>
