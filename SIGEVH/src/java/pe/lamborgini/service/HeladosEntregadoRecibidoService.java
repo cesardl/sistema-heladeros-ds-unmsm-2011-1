f457ab1d033380b546eb953eb596190bc9eeeef4
cesar.dl88@gmail.com <cesar.dl88@gmail.com@c57c525a-3481-9d06-e16f-5780e9421b25>
2011-07-01 05:43:03 +0000

    Mejora del DAO y Service de almacenamiento de helados asignados

--- SIGEVH/src/java/pe/lamborgini/service/HeladosEntregadoRecibidoService.java
+++ SIGEVH/src/java/pe/lamborgini/service/HeladosEntregadoRecibidoService.java
@@ -4,8 +4,14 @@
  */
 package pe.lamborgini.service;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import pe.lamborgini.dao.HeladosEntregadoRecibidoDAO;
+import pe.lamborgini.domain.mapping.DetalleHelado;
+import pe.lamborgini.domain.mapping.Heladero;
 import pe.lamborgini.domain.mapping.HeladosEntregadoRecibido;
+import pe.lamborgini.util.AppUtil;
 
 /**
  *
@@ -13,7 +19,24 @@ import pe.lamborgini.domain.mapping.HeladosEntregadoRecibido;
  */
 public class HeladosEntregadoRecibidoService {
 
-    public static void guardarHeladosEntregadoRecibido(HeladosEntregadoRecibido her) {
+    public static void guardarHeladosEntregadoRecibido(List<DetalleHelado> listaDetalleHelados, String id_heladero) {
+        HeladosEntregadoRecibido her = new HeladosEntregadoRecibido();
+
+        her.setFecha(new Date());
+        her.setHeladero(new Heladero(AppUtil.aInteger(id_heladero)));
+
+        double total = 0;
+
+        for (int i = 0; i < listaDetalleHelados.size(); i++) {
+            DetalleHelado dh = listaDetalleHelados.get(i);
+            dh.setHeladosEntregadoRecibido(her);
+            total = total + dh.getCantEntregada();
+        }
+        her.setDetalleHelados(new HashSet<DetalleHelado>(listaDetalleHelados));
+        her.setTotal(total);
+
+        System.out.println("total : " + total + "-" + listaDetalleHelados.size());
+
         new HeladosEntregadoRecibidoDAO().insertHeladosEntregadoRecibido(her);
     }
 }
