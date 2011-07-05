58fb0c142dc26bfcc78263f86fa08367b2fec6c1
cesar.dl88@gmail.com <cesar.dl88@gmail.com@c57c525a-3481-9d06-e16f-5780e9421b25>
2011-07-05 00:51:25 +0000

    Mejora en la busqueda de heladeros y filtro en la devolucion de helados

--- SIGEVH/database/heladeros.sql
+++ SIGEVH/database/heladeros.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: heladeros
 Target Host: localhost
 Target Database: heladeros
-Date: 03/07/2011 11:28:59 p.m.
+Date: 04/07/2011 07:17:20 p.m.
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -66,7 +66,7 @@ CREATE TABLE `detalle_helado` (
   CONSTRAINT `fk_detalle_helado_helado1` FOREIGN KEY (`id_helado`) REFERENCES `helado` (`id_helado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_detalle_helado_helados_entregado_recibido1` FOREIGN KEY (`id_helados_entregado_recibido`) REFERENCES `helados_entregado_recibido` (`id_helados_entregado_recibido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_detalle_helado_pago_helado1` FOREIGN KEY (`id_pago_helado`) REFERENCES `pago_helado` (`idpago_helado`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for factura
@@ -79,7 +79,7 @@ CREATE TABLE `factura` (
   `descripcion` varchar(45) NOT NULL,
   `pago` double NOT NULL,
   PRIMARY KEY  (`id_factura`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for heladero
@@ -118,7 +118,7 @@ CREATE TABLE `helados_entregado_recibido` (
   PRIMARY KEY  (`id_helados_entregado_recibido`),
   KEY `fk_helado_entregado_recibido_heladero1` (`id_heladero`),
   CONSTRAINT `fk_helado_entregado_recibido_heladero1` FOREIGN KEY (`id_heladero`) REFERENCES `heladero` (`id_heladero`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for pago_helado
@@ -134,7 +134,7 @@ CREATE TABLE `pago_helado` (
   KEY `fk_pago_helado_concepto1` (`id_concepto`),
   CONSTRAINT `fk_pago_helado_concepto1` FOREIGN KEY (`id_concepto`) REFERENCES `concepto` (`id_concepto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_pago_helado_factura1` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id_factura`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for stock_helado
@@ -169,10 +169,10 @@ CREATE TABLE `usuario` (
 -- ----------------------------
 INSERT INTO `concepto` VALUES ('1', 'Pago por venta de helados');
 INSERT INTO `concesionario` VALUES ('1', 'Adex', 'Ate-Vitarte', 'Luis Hanampa'), ('2', 'GYH', 'Comas', 'Edgar Cordova'), ('3', 'Frigolac', 'Callao', 'Mariana Rivero'), ('4', 'PyT', 'Barranco', 'Javier Lorenao'), ('5', 'FerD', 'Jesus Maria', 'Delia Martinez');
-INSERT INTO `detalle_helado` VALUES ('1', '13', '1', '7', '30', '1', '29'), ('2', '8', '1', '6', '10', '2', '8'), ('3', '11', '1', '8', '12', '2', '10'), ('4', '17', '2', '4', '20', '3', '17'), ('5', '11', '2', '3', '20', '2', '18'), ('6', '9', '2', '5', '11', '1', '10'), ('7', '9', '3', null, '23', '0', '0'), ('8', '11', '4', null, '20', '0', '0'), ('9', '11', '5', '2', '20', '3', '17'), ('10', '9', '5', '1', '12', '2', '10'), ('15', '8', '7', null, '17', '0', '0'), ('16', '9', '7', null, '20', '0', '0'), ('17', '13', '7', null, '21', '0', '0'), ('18', '11', '7', null, '27', '0', '0');
-INSERT INTO `factura` VALUES ('1', '6812325', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '66'), ('2', '2564848', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '86'), ('3', '9035400', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '104');
+INSERT INTO `detalle_helado` VALUES ('1', '13', '1', '7', '30', '1', '29'), ('2', '8', '1', '6', '10', '2', '8'), ('3', '11', '1', '8', '12', '2', '10'), ('4', '17', '2', '4', '20', '3', '17'), ('5', '11', '2', '3', '20', '2', '18'), ('6', '9', '2', '5', '11', '1', '10'), ('7', '9', '3', null, '23', '0', '0'), ('8', '11', '4', null, '20', '0', '0'), ('9', '11', '5', '2', '20', '3', '17'), ('10', '9', '5', '1', '12', '2', '10'), ('15', '8', '7', null, '17', '0', '0'), ('16', '9', '7', null, '20', '0', '0'), ('17', '13', '7', null, '21', '0', '0'), ('18', '11', '7', null, '27', '0', '0'), ('19', '11', '8', '9', '20', '2', '18'), ('20', '2', '8', '10', '10', '4', '6'), ('21', '1', '8', '12', '20', '1', '19'), ('22', '8', '8', '11', '10', '2', '8'), ('23', '8', '9', '14', '20', '2', '18'), ('24', '11', '9', '13', '15', '2', '13'), ('25', '2', '9', '15', '15', '0', '15');
+INSERT INTO `factura` VALUES ('1', '6812325', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '66'), ('2', '2564848', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '86'), ('3', '9035400', '2011-07-03', 'Venta de helados a fecha 03-07-2011', '104'), ('4', '8289500', '2011-07-04', 'Venta de helados a fecha 04-07-2011', '126.5'), ('5', '1125900', '2011-07-04', 'Venta de helados a fecha 04-07-2011', '97.5');
 INSERT INTO `heladero` VALUES ('1', '1', 'Mario', 'Ramirez Coronado'), ('2', '1', 'Jose David', 'Torres Cordova'), ('3', '1', 'Mariana', 'Quispe Delgado'), ('4', '1', 'Lady Diana', 'Saavedra Luque'), ('5', '1', 'Francisco ', 'Gonzales Diaz'), ('6', '2', 'Daniela', 'Zapata Cruz'), ('7', '2', 'Juan Esteban', 'Gamarra Desposorio'), ('8', '2', 'Miguel ', 'Zarate Abanto'), ('9', '3', 'Flor de Maria', 'Izquierdo Morales'), ('10', '3', 'Ana Cristina', 'Vadillo Montes'), ('11', '3', 'Luis Enrique', 'Bustamante Montoya'), ('12', '4', 'Cesar ', 'Salinas Romero'), ('13', '4', 'Yasmin Liset', 'Carranza Lopez'), ('14', '4', 'Eduardo ', 'Atoche Zapata'), ('15', '4', 'Ulises ', 'Elguera Gallo'), ('16', '5', 'Felipe Otoniel', 'Coronel Pedreros'), ('17', '5', 'Denisse ', 'Infantes Morales'), ('18', '5', 'Miguel ', 'Luyo Pineda'), ('19', '5', 'Denisse Katherine', 'Palomino Astupuma'), ('20', '5', 'Juan Luis', 'Hurtado Medina');
 INSERT INTO `helado` VALUES ('1', 'Carnevale fresa', '2.5'), ('2', 'Oasu', '1.5'), ('3', 'Copa viva vainilla', '3.5'), ('4', 'Piccolo fresa', '1'), ('5', 'Frutarello limon', '1'), ('6', 'DPelicula', '4'), ('7', 'Lamborgini Light', '3.5'), ('8', 'Tartufo', '2'), ('9', 'Tartufo Clasico', '1.5'), ('10', 'Tartuffo Pecatto', '2'), ('11', 'Bombones Choconum', '3'), ('12', 'Sandwich vainilla pequeño', '1'), ('13', 'Sandwich choconieve grande ', '2'), ('14', 'Sandiwch vainilla grande', '2'), ('15', 'Carnavale chocolate', '2.5'), ('16', 'Vip mora', '1'), ('17', 'Vip mango ', '1'), ('18', 'Copa viva lucuma - vainilla', '3.5'), ('19', 'Copa viva mermelada fresa', '3.5'), ('20', 'Piccolo chica', '1'), ('21', 'Piccolo manzana', '1'), ('22', 'Frutarello guanabana', '1'), ('23', 'Frutarello coco', '1'), ('24', 'Pionono vainilla', '4.5'), ('25', 'Pionono lucuma', '4.5'), ('26', 'Casino chocolate', '1.5'), ('27', 'Casino lucuma', '1.5');
-INSERT INTO `helados_entregado_recibido` VALUES ('1', '2', '2011-07-02', '52'), ('2', '5', '2011-07-03', '51'), ('3', '7', '2011-07-03', '23'), ('4', '13', '2011-07-03', '20'), ('5', '3', '2011-07-03', '32'), ('7', '2', '2011-07-03', '85');
-INSERT INTO `pago_helado` VALUES ('1', '1', '1', '15'), ('2', '1', '1', '51'), ('3', '2', '1', '54'), ('4', '2', '1', '17'), ('5', '2', '1', '15'), ('6', '3', '1', '16'), ('7', '3', '1', '58'), ('8', '3', '1', '30');
+INSERT INTO `helados_entregado_recibido` VALUES ('1', '2', '2011-07-02', '52'), ('2', '5', '2011-07-03', '51'), ('3', '7', '2011-07-03', '23'), ('4', '13', '2011-07-03', '20'), ('5', '3', '2011-07-03', '32'), ('7', '2', '2011-07-03', '85'), ('8', '1', '2011-07-04', '60'), ('9', '2', '2011-07-04', '50');
+INSERT INTO `pago_helado` VALUES ('1', '1', '1', '15'), ('2', '1', '1', '51'), ('3', '2', '1', '54'), ('4', '2', '1', '17'), ('5', '2', '1', '15'), ('6', '3', '1', '16'), ('7', '3', '1', '58'), ('8', '3', '1', '30'), ('9', '4', '1', '54'), ('10', '4', '1', '9'), ('11', '4', '1', '16'), ('12', '4', '1', '47'), ('13', '5', '1', '39'), ('14', '5', '1', '36'), ('15', '5', '1', '22');
 INSERT INTO `usuario` VALUES ('1', '1', 'LHanampa', 'luis', 'Gerente'), ('2', '2', 'ECordova', 'edgar', 'Jefe de Ventas'), ('3', '3', 'MRivero', 'mariana', 'Asistente'), ('4', '4', 'JLorenao', 'javier', 'Gerente'), ('5', '5', 'DMartinez', 'delia', 'Contador'), ('6', '1', 'admin', 'admin', 'Administrador');
